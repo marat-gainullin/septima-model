6d9ad63d41958bdee6aeb81d835bbf9f8d596bbb
marat-gainullin <mg@altsoft.biz>
2017-10-26 22:42:00 +0300

    Gulpfile fix. Bundle task removed

--- karma.conf.js
+++ karma.conf.js
@@ -38,9 +38,6 @@ module.exports = function (config) {
             ],
             extensions: ['.js']
         },
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: true,
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
@@ -70,6 +67,9 @@ module.exports = function (config) {
         browsers: ['Chrome'],
         // Concurrency level
         // how many browser should be started simultaneous
-        concurrency: Infinity
+        concurrency: Infinity,
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true
     });
 };
